ebe8b83f5dabc2d1b0aac6fb4fc27f2a384cf01b
Patri <patricia@zapp-studio.com>
2023-11-30 09:15:14 +0100

    Add composition API 01 resolved assignment

--- Assignments/composition-assignment-1-problem/src/App.vue
+++ Assignments/composition-assignment-1-problem/src/App.vue
@@ -2,9 +2,9 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <h3 v-if="hidden">{{ goal }}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button>Toggle Goal</button>
+  <button @click="toggleGoal">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -13,8 +13,68 @@
 </template>
 
 <script>
+// import {ref} from 'vue'
+// import {reactive} from 'vue'
+import {reactive, toRefs} from 'vue'
 export default {
+
+  /* 4 --- Object API solution */
+  // data() {
+  //   return {
+  //     goal: 'Hola',
+  //     hidden: false, 
+  //   },
+  // },
+
+  // methods: {
+  //   hideGoal = () => {
+  //       this.hidden = !this.hidden;
+  //   }
+  //   },
   
+
+  setup(){
+
+
+/*     1 -- Using only Refs solution */
+
+    // const courseGoal = ref('Hola')
+    // let hidden = ref(false);
+
+    // const hideGoal = () => {
+    //     hidden.value = !hidden.value;
+    // }
+
+    // return { courseGoal, toggleGoal: hideGoal, hidden: hidden  }
+
+/*     2 -- Using only Reactive solution */
+
+  //   const courseData = reactive({
+  //     goal: 'Hola',
+  //     hidden: false
+  //   })
+
+  //   const hideGoal = () => {
+  //       courseData.hidden = !courseData.hidden;
+  //   }
+
+  //   return { courseData, toggleGoal: hideGoal }
+  // }
+
+/*     3 -- Using toRefs solution */
+    const courseData = reactive({
+      goal: 'Hola',
+      hidden: false
+    })
+
+    const courseRefs = toRefs(courseData);
+
+    const hideGoal = () => {
+        courseData.hidden = !courseData.hidden;
+    }
+
+    return { courseData, toggleGoal: hideGoal, goal:courseRefs.goal, hidden: courseRefs.hidden }
+  }
 }
 </script>
 
