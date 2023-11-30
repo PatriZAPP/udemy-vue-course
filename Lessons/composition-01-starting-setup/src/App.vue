9f4442b56bf3bd7dc7538ab06a7324edb60d982e
Patri <patricia@zapp-studio.com>
2023-11-30 10:13:37 +0100

    how to use computed and watchers wit Composition API

--- Lessons/composition-01-starting-setup/src/App.vue
+++ Lessons/composition-01-starting-setup/src/App.vue
@@ -1,31 +1,53 @@
 <template>
   <section class="container">
-    <h2>{{ user.name }}</h2>
-    <h3>{{ user.age }}</h3>
+    <h2>{{ fullName }}</h2>
+    <h3>{{ uAge }}</h3>
 <button @click="setAge">Change Age</button>
+<div>
+  <input type="text" placeholder="First name" v-model="firstName">
+  <input type="text" placeholder="Last name" v-model="lastName">
+</div>
   </section>
 </template>
 
 <script>
-import { reactive  } from 'vue';
+import { ref, computed, watch  } from 'vue';
 
 export default {
 setup() {
-  const user = reactive({
-    name: 'Maximilian',
-    age: 31
+  // const user = reactive({
+  //   name: 'Maximilian',
+  //   age: 31
+  // })
+
+  const firstName = ref('');
+  const lastName = ref('');
+  const uAge = ref(31);
+
+  // const setFirstName = (event) => {
+  //   firstName.value = event.target.value 
+  // }
+  // const setLastName = (event) => {
+  //   lastName.value = event.target.value
+  // }
+
+const fullName = computed(() => {
+  return firstName.value + ' ' + lastName.value;
+})
+
+  watch([fullName, uAge], function (newValues, oldValues){
+    console.log('Old age:' + oldValues[0]);
+    console.log('New age:' + newValues[0]);
+    console.log('Old name:' + oldValues[1]);
+    console.log('New name:' + newValues[1]);
+
   })
 
   const setNewAge = () => {
-    user.age = 32;
+    uAge.value = 32;
   }
-  return { user: user, setAge: setNewAge };
+  return { uAge, fullName, firstName, lastName, setAge: setNewAge };
 }
-  // data() {
-  //   return {
-  //     userName: 'Maximilian',
-  //   };
-  // },
 };
 </script>
 
